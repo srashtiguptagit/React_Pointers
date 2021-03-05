413e145e1b7bae8c10a4f2b1f6166390d45a1d58
srashtiguptagit <srashtigupta1708@gmail.com>
2021-03-05 09:58:48 +0530

    Function calls in react

---
+++ reactapp/src/components/Person/Person.js
@@ -0,0 +1,22 @@
+// This is a functional component
+// These are the functional components . Till React 16.8 it was not possible to maintain state in 
+// functional components now its possible with React hooks
+import React, {useState} from 'react';
+
+const Person = (props) => {
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+     //As state in class based componensts has to be objcets here it can be anything
+    return (
+        <div>
+            <p>My name is {props.name} and I am {props.age} years old</p>
+            <p>My name is {props.name} and I am {props.age} years old</p>
+            <p>My name is {props.click} </p>
+            <input type="text" value={enteredTitle} onChnage={event => {setEnteredTitle(event.target.value)}}/>
+            <input type="text" value={enteredAmount} onChnage={event => {setEnteredAmount(event.target.value)}}/>
+        </div>
+    )
+}
+
+export default Person;
+
--- reactapp/src/containers/Persons/Persons.js
+++ reactapp/src/containers/Persons/Persons.js
@@ -1,25 +1,32 @@
 // Class based component
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import classes from './Persons.css';
 
 class Persons extends Component {
     //state is always an object and can be used only for class based components
-    state= {
-        persons:[
+    state = {
+        persons: [
             {
                 name: 'Srashti', age: 31
             },
             {
-                name:'Miraya', age: 3
+                name: 'Miraya', age: 3
             }
         ]
     }
-    render(){
+    clickHandler = () => {
+        console.log('I got clicked');
+
+    }
+    render() {
         return (
             <div className={classes.Persons}>
-                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}
+                    click={this.clickHandler.bind(this, "Mirri")} />
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+                <Button onClick={this.clickHandler.bind(this, "Mac")}></Button>
+                <Button onClick={() => {this.clickHandler('Srashti')}}></Button>
             </div>
         )
     }
